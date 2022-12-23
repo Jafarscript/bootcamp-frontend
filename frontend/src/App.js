fab5d71b69abcbf823ef79a915aedd5c1ebf6f88
Jafarscript <lihammedjafar@gmail.com>
2022-12-23 12:26:00 +0100

    LogIn page and simple authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import Login from './components/Login/Login';
 
 function App() {
+  const [user, setUser] = useState(false)
   return (
     <div className="App">
-      <Login />
+      { !user ? <Login  setUser={setUser} /> : (
+        <h1>User Logged in</h1>
+      )}
     </div>
   );
 }
--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,17 +1,36 @@
 import React,{useState} from 'react'
 
-const Login = () => {
-  const [value, setValue] = useState({
-    name: '',
-    password: ''
-  })
+const Login = ({setUser}) => {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const [show, setShow] = useState(true)
 
-  const handleChange = (e) => {
-    setValue({...value, [e.target.name] : e.target.value })
+  const [err, setErr] = useState(false)
+
+  const [errEmail, setErrEmail] = useState(false)
+
+  const click = () => {
+    setShow(!show)
   }
 
-  const handleSubmit = () => {
-    
+  const handleClick = () => {
+    if( !password || password.length < 8){
+      setErr(!err)
+    } else {
+      setErr()
+    }
+    if(!email || email.includes('@') === false){
+      setErrEmail(!errEmail)
+    }else {
+      setErrEmail()
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(email.includes('@') && password.length > 8){
+      setUser(true)
+    }
   }
   return (
     <section className='h-screen grid grid-cols-2'>
@@ -23,10 +42,16 @@ const Login = () => {
             <h3 className='text-[40px] text-[#213F7D] font-bold leading-[55px]'>Welcome!</h3>
             <h6 className='mt-[10px] text-[20px] font-extralight'>Enter details to login.</h6>
             <form action="" className='mt-[60px] flex flex-col gap-[24px] mr-[57px]' onSubmit={handleSubmit}>
-              <input type="email" placeholder='Email' className='p-[17px] text-[#545F7D] rounded-[5px] text-[14px] border-2 border-[rgba(84,95,125, 0.15)] '  value={value.name} onChange={handleChange}/>
-              <input type="password" placeholder='Password' value={value.password} className='p-[17px] text-[#545F7D] rounded-[5px] text-[14px] border-2 border-[rgba(84,95,125, 0.15)]' onChange={handleChange}/>
+              <input type="email" placeholder='Email' className='p-[17px] text-[#545F7D] rounded-[5px] text-[14px] border-2 border-[rgba(84,95,125, 0.15)] '  value={email} onChange={e => setEmail(e.target.value)}/>
+             <p className={errEmail ? 'block text-red-700' : 'hidden'}>Invalid Email Address</p>
+
+             <div className='w-full relative'>
+             <input type={show ? 'password' : 'text'} placeholder='Password' value={password} className='p-[17px] text-[#545F7D] rounded-[5px] text-[14px] border-2 border-[rgba(84,95,125, 0.15)] w-full' onChange={e => setPassword(e.target.value)}/>
+             {show ? <p className='absolute right-6 bottom-5 uppercase text-[#39CDCC] text-[12px] cursor-pointer font-semibold' onClick={click} >Show</p> : <p className='absolute right-6 bottom-5 uppercase text-[#39CDCC] text-[12px] cursor-pointer font-semibold' onClick={click} >Hide</p> }
+             </div>
+             <p className={err ? 'block text-red-700' : 'hidden'}>Password length should be more than 8</p>
               <p className='text-[12px] text-[#39CDCC] font-semibold uppercase'>Forgotten Password ?</p>
-              <button className='uppercase text-[14px] p-[15px] rounded-[8px] bg-[#39CDCC] text-white'>log in</button>
+              <button className='uppercase text-[14px] p-[15px] rounded-[8px] bg-[#39CDCC] text-white' onClick={handleClick}>log in</button>
             </form>
         </div>
     </section>
