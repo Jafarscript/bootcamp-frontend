2e684447ed0d51f07f127232dd385d6d904f050b
Jafarscript <lihammedjafar@gmail.com>
2022-12-23 12:57:57 +0100

    LogIn page and simple authentication (Done and Responsive)

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -14,6 +14,7 @@ const Login = ({setUser}) => {
   }
 
   const handleClick = () => {
+
     if( !password || password.length < 8){
       setErr(!err)
     } else {
@@ -33,16 +34,16 @@ const Login = ({setUser}) => {
     }
   }
   return (
-    <section className='h-screen grid grid-cols-2'>
-        <div className='pt-28 px-20'>
+    <section className='h-screen grid gap-14 place-content-between grid-rows-2 p-10 lg:p-0 lg:grid-cols-2 lg:gap-0 lg:place-content-stretch'>
+        <div className='place-self-center lg:pt-28 lg:px-20 lg:place-self-stretch'>
           <img src="https://res.cloudinary.com/dvo8xhx6r/image/upload/v1671570298/Group_ji3ybk.svg" alt="logo"  className='mb-36'/>
           <img src="https://res.cloudinary.com/dvo8xhx6r/image/upload/v1671570337/pablo-sign-in_1_mdgqhu.svg" alt="illustration" />
         </div>
-        <div className='pt-[220px] flex flex-col p-[100px]'>
+        <div className=' flex flex-col lg:pt-[220px] lg:p-[100px] place-self-center lg:place-self-stretch w-full'>
             <h3 className='text-[40px] text-[#213F7D] font-bold leading-[55px]'>Welcome!</h3>
             <h6 className='mt-[10px] text-[20px] font-extralight'>Enter details to login.</h6>
-            <form action="" className='mt-[60px] flex flex-col gap-[24px] mr-[57px]' onSubmit={handleSubmit}>
-              <input type="email" placeholder='Email' className='p-[17px] text-[#545F7D] rounded-[5px] text-[14px] border-2 border-[rgba(84,95,125, 0.15)] '  value={email} onChange={e => setEmail(e.target.value)}/>
+            <form action="" className='mt-[20px] lg:mt-[60px] flex flex-col gap-[20px] lg:mr-[57px] ' onSubmit={handleSubmit}>
+              <input type="email" placeholder='Email' className=' p-[17px] text-[#545F7D] rounded-[5px] text-[14px] border-2 border-[rgba(84,95,125, 0.15)] '  value={email} onChange={e => setEmail(e.target.value)}/>
              <p className={errEmail ? 'block text-red-700' : 'hidden'}>Invalid Email Address</p>
 
              <div className='w-full relative'>
